5bb37f38e2806787f9fbf41af4b68e22507d48c4
Morgan Delaney <morganthetall@gmail.com>
2014-08-16 16:01:51 -0700

    Added mail wsdl for Junction Pub temporary fix

---
+++ api/client.php
@@ -0,0 +1,6 @@
+<?php
+// Example!
+//ini_set("soap.wsdl_cache_enabled", 0);
+//$client = new SoapClient( 'http://localhost/morgan/api/?wsdl', array( 'trace' => 1, 'soap_version' => SOAP_1_2 ) );
+//
+//echo $client->mail( 'morganthetall@gmail.com', '[Junction Pub] New contact form entry', 'My message goes here' );
