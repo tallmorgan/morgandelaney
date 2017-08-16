928e3c3bafc05eb8eaa2b8f277b008e8781b29d7
Morgan Delaney <hi@morgandelaney.me>
2017-08-16 15:07:09 +0200

    Merge simple version into this repo

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-morgandelaney.me
-================
+# morgandelaney.me
 
 This is a simply repo for browsing the code behind morgandelaney.me built in WordPress.
+
+## Routing to full/simple pages
+
+To form a whitelist to dis/allow routes to the full/simple pages, include the following
+line at the top of index.php:
+
+```
+if (!isset($_GET['full'])) die(include('index-simple.php'));
+```
---
+++ index-simple.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+    <title>Morgan Delaney</title>
+    <link href="//fonts.googleapis.com/css?family=Roboto:100,500" rel="stylesheet" type="text/css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="simple/favicon.png" />
+    <link rel="stylesheet" href="simple/style.css" />
+</head>
+<body>
+<div class="shadow"></div>
+<div class="content">
+    <div class="valign"></div>
+    <div class="message">
+        <img src="simple/howdy.jpg" alt="" />
+        <h1>Morgan Delaney</h1>
+        <h2>source whisperer</h2>
+        <h3><i class="fa fa-envelope"></i>: <a href="mailto:hi@morgandelaney.me">hi@morgandelaney.me</a></h3>
+    </div>
+</div>
+</body>
+</html>
