b90820bfd425951bec1d67261d076c7d5bb29c75
Morgan Delaney <hi@morgandelaney.me>
2017-08-16 20:24:39 +0200

    Install livereload snippet

--- index-full.php
+++ index-full.php
@@ -218,6 +218,13 @@
 <script type="text/javascript" src="full/js/imagesloaded.pkgd.min.js"></script>
 <script type="text/javascript" src="full/js/functions.js"></script>
 
+<?php if (isset($_GET['livereload'])): ?>
+    <script>
+        document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] +
+            ':35729/livereload.js?snipver=1"></' + 'script>')
+    </script>
+<?php endif; ?>
+
 </body>
 
 </html>
